cb5866b94dd6cfdef8ab4268c1890c16ddbf2b9c
9w107 <vaclav.vancura07@gmail.com>
2024-09-30 16:55:10 +0200

    main screen upgrade

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -6,29 +6,120 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          scaffoldBackgroundColor: const Color(0xFF000000),
+          fontFamily: 'Archabeta',
+          navigationBarTheme: const NavigationBarThemeData(
+            backgroundColor: Color(0xffff4c00),
+            indicatorColor: Color(0x00000000),
+            shadowColor: Color(0x00000000),
+            surfaceTintColor: Color(0x00000000),
+          ),
+          appBarTheme: const AppBarTheme(
+              color: Color(0xFFff4c00),
+              foregroundColor: Color(0xFF000000),
+              titleTextStyle: TextStyle(
+                  color: Color(0xFF000000),
+                  fontFamily: 'Archabeta',
+                  fontWeight: FontWeight.w700,
+                  fontSize: 40))),
+      home: const RootPage(),
+    );
+  }
+}
+
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+
+  int page = 0;
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300],
       appBar: AppBar(
-        title: const Text('Hlavní strana', style: TextStyle(color: Colors.black)),
-        backgroundColor: Colors.grey[300],
-        centerTitle: true,
-        actions: [
-          IconButton(
-            icon: const CircleAvatar(
-              child: Icon(Icons.person), // Profile icon
-            ),
-            onPressed: () {
+        title: Row(
+          mainAxisAlignment:
+              MainAxisAlignment.spaceBetween,
+          children: [
+            const Text("ARCHA+"),
+            FlashTextWidget(label: ' MENU', onTapMessage: 'MENU', size: 40, onTap: () {},),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        height: 50,
+        color: const Color(0xFFFF4C00),
+        child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [            
+            FlashTextWidget(label: ' HOME ', onTapMessage: 'HOME', size: 30, onTap: () {},),
+            FlashTextWidget(label: ' USER ', onTapMessage: 'USER', size: 30, onTap: () {
               // Navigate to user settings screen
               Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context) => const UserSettingsScreen()),
               );
-            },
-          ),
-        ],
-      ),
-      body: const Center(
-        child: Text('Vítej!'),
+            },),
+            FlashTextWidget(label: ' ABOUT ', onTapMessage: 'ABOUT', size: 30, onTap:() {},),
+            ]
+        ),
+      )
+    );
+  }
+}
+
+class FlashTextWidget extends StatefulWidget {
+  final String label;
+  final String onTapMessage;
+  final double? size;
+  final VoidCallback onTap;
+  const FlashTextWidget({super.key, required this.label, required this.onTapMessage, required this.size, required this.onTap});
+
+  @override
+  _FlashTextWidgetState createState() => _FlashTextWidgetState();
+}
+
+class _FlashTextWidgetState extends State<FlashTextWidget> {
+  Color _currentColor = const Color(0xFF000000);
+  final Color _flashColor = const Color(0xFF7F2500);
+
+  void _onTapDown(TapDownDetails details) {
+    setState(() {
+      _currentColor = _flashColor;
+    });
+    debugPrint('${widget.onTapMessage} TAPPED');
+  }
+
+  void _onTapUp(TapUpDetails details) {
+    setState(() {
+      _currentColor = const Color(0xFF000000);
+    });
+    widget.onTap();
+    debugPrint('${widget.onTapMessage} TAP RELEASED');
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: _onTapDown,
+      onTapUp: _onTapUp,
+      onTapCancel: () {
+        setState(() {
+          _currentColor = const Color(0xFF000000);
+        });
+      },
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 150),
+        child: Text(
+          widget.label,
+          style: TextStyle(fontSize: widget.size, color: _currentColor),
+        ),
       ),
     );
   }
