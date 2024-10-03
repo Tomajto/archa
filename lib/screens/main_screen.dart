d9494420d95fdac4d6a522f699f44a334d0238d4
Tomajto <tomajto47@gmail.com>
2024-10-03 20:46:52 +0200

    app icon

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'user_settings_screen.dart'; // Import the user settings screen
 import 'chat_screen.dart'; // Import the chat screen
